b8d0d545f8924110035d24a68ba51e2a7af18e43
Elvis <elvisgiltorres@gmail.com>
2022-06-02 19:33:07 -0400

    Update footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -11,13 +11,17 @@ const networkLinks = [
 export const Footer = () => {
     return (
         <div className="bg-red-600 shadow w-full h-28 absolute inset-y-full text-white text-sm">
-            <div className="flex  h-20 flex-row justify-evenly items-center"> 
-                {
-                    networkLinks.map(item => (
-                        <a href={item.href} className="py-2 pr-4 pl-3">{item.name}</a>
-                    ))
-                }
-            </div>
+            <div className="max-w-6xl mx-auto px-5 xl:px-0 h-14 flex items-center justify-center">
+                <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium"> 
+                    {
+                        networkLinks.map(item => (
+                            <li key={item.id}>
+                                <a href={item.href} className="py-2 pr-4 pl-3">{item.name}</a>
+                            </li>
+                        ))
+                    }
+                </ul>
+                </div>
             <h3 className="text-center italic">All right reserved</h3>
         </div>
     )
