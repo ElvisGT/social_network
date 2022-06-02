748a0b184eda474ff9d0018c83ea04a16a507000
Elvis <elvisgiltorres@gmail.com>
2022-06-02 18:44:07 -0400

    Footer ready

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 
-const NetworkLinks = [
+const networkLinks = [
     {id:1,href:"exaple.com",name:"Linkedin"},
     {id:2,href:"exaple.com",name:"Github"},
     {id:3,href:"exaple.com",name:"Facebook"},
@@ -10,8 +10,15 @@ const NetworkLinks = [
 
 export const Footer = () => {
     return (
-        <div className="bg-red-600 w-full h-8 relative bottom-full">
-            
+        <div className="bg-red-600 shadow w-full h-28 absolute inset-y-full text-white text-sm">
+            <div className="flex  h-20 flex-row justify-evenly items-center"> 
+                {
+                    networkLinks.map(item => (
+                        <a href={item.href} className="py-2 pr-4 pl-3">{item.name}</a>
+                    ))
+                }
+            </div>
+            <h3 className="text-center italic">All right reserved</h3>
         </div>
     )
 }
