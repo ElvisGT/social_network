8e865ac530e19c178d373e223528308d6c4f5999
Elvis <elvisgiltorres@gmail.com>
2022-06-02 16:42:05 -0400

    Test version of home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,14 @@ const Home: NextPage = () => {
       <div>
           <div className="flex flex-col items-center pt-12">
             <h1 className="text-3xl">Welcome to the</h1>
-            <p className="text-8xl pt-3 text-red-600">FUTURE</p>
-            <p className="w-80 ml-8 mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam provident voluptatum quaerat quisquam nulla quae tenetur? Consectetur sit totam vero veritatis blanditiis ex ipsam quae, ut corporis optio necessitatibus. Dolorem.</p> 
+            <p className="text-8xl pt-3 text-red-600 font-bold ">DEV</p>
+            <p className="text-8xl pt-3 text-red-600 font-bold">COMMUNITY</p>
+            <p className="w-80 ml-8 mt-3">The best site for connect with more developers</p> 
           </div>
             <div className="flex justify-center p-8">
               <div className="flex justify-evenly w-80">
                 <Button className="flex item-center justify-center px-4 w-auto bg-red-600 h-7 rounded-3xl active:border-2 hover:bg-red-500 text-white">
-                    Click me!
+                    Enter
                 </Button>
                 <Button className="flex item-center justify-center px-4 w-auto bg-red-600 h-7 rounded-3xl active:border-2 hover:bg-red-500 text-white">
                     Why us?
