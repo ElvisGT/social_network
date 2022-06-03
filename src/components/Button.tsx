e56fd76ab64007e3f0f5390037b75092e2cfc37a
JancoBH <jancoboscan@gmail.com>
2022-06-03 00:18:15 -0300

    mini fixes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,20 @@
 export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>,HTMLButtonElement>,React.AriaAttributes {}
 
+export const Button: React.FC<ButtonProps> = (
+  {
+    children,
+    className,
+    ...rest
+  }) => {
 
-export const Button  : React.FC<ButtonProps> = ({children,className,...rest}) => {
-    const classDefault = "flex item-center justify-center px-4 w-auto bg-red-600 h-7 rounded-3xl active:border-2 hover:bg-red-500 text-white"
-    return (
-        <button 
-            {...rest}
-            className={className ? className : classDefault}
-            >
-            {children}
-        </button>
-    )
+  const defaultBtn = "flex items-center justify-center px-4 w-auto bg-red-600 h-7 rounded-3xl active:border-2 hover:bg-red-500 text-white m-1";
+
+  return (
+    <button
+      className={`${defaultBtn} ${className}`}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,26 +5,24 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div>
-          <div className="flex flex-col items-center pt-12">
-            <h1 className="text-3xl">Welcome to the</h1>
-            <p className="text-8xl pt-3 text-red-600 font-bold ">DEV</p>
-            <p className="text-8xl pt-3 text-red-600 font-bold">COMMUNITY</p>
-            <p className="w-80 ml-8 mt-3">The best site for connect with more developers</p> 
-          </div>
-            <div className="flex justify-center p-8">
-              <div className="flex justify-evenly w-80">
-                <Button>
-                    Enter
-                </Button>
-                <Button>
-                    Why us?
-                </Button>
-              </div>
-            </div>
-            
+      <section>
+        <div className="flex flex-col items-center pt-12">
+          <h1 className="text-3xl">Welcome to the</h1>
+          <p className="text-8xl pt-3 text-red-600 font-bold">DEV</p>
+          <p className="text-8xl pt-3 text-red-600 font-bold">COMMUNITY</p>
+          <p className="w-80 ml-8 mt-3">The best site for connecting with more developers</p>
+        </div>
 
-      </div>
+        <div className="flex justify-center p-8">
+          <Button>
+            Enter
+          </Button>
+          <Button>
+            Why us?
+          </Button>
+        </div>
+
+      </section>
     </>
   )
 }
